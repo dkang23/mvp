809dfdfaa58c9d43f4c1cf0c4be6a30e5931f3f3
Daniel Kang <dkang23@gmail.com>
2020-10-28 16:12:01 -0400

    front-end

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
 const puppeteer = require('puppeteer');
 const moment = require('moment');
 
-var keepGoing = true;
-
+var counter = 0;
 (async () => {
-  while (keepGoing) {
+  var keepGoing = true;
+  while (keepGoing && moment().minute() < 60) {
     //args: ['--no-sandbox']
+    console.log(keepGoing, moment().minute());
+
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
-    await page.goto('https://direct.activegolf.com/home.asp?WSID=1058');
-    await page.goto('https://direct.activegolf.com/crs/GetTeeTime.asp?SSL=1');
-    await page.$eval(
-      'input[name=MemberAccessNum]',
-      (el) => (el.value = '37975723')
-    );
-    await page.$eval('input[name=MemberPIN]', (el) => (el.value = '1431'));
-    await page.$eval('input[name=btnSubmit]', (button) => button.click());
+    await page
+      .goto('https://direct.activegolf.com/home.asp?WSID=1058')
+      .catch((err) => console.err);
+    await page
+      .goto('https://direct.activegolf.com/crs/GetTeeTime.asp?SSL=1')
+      .catch((err) => console.err);
+    await page
+      .$eval('input[name=MemberAccessNum]', (el) => (el.value = '37975723'))
+      .catch((err) => console.err);
+    await page
+      .$eval('input[name=MemberPIN]', (el) => (el.value = '1431'))
+      .catch((err) => console.err);
+    await page
+      .$eval('input[name=btnSubmit]', (button) => button.click())
+      .catch((err) => console.err);
 
     await page.waitFor(500);
     await page
@@ -33,35 +42,50 @@ var keepGoing = true;
     // await page
     //   .$eval('input[name=QTRSFacilityID6]', (check) => (check.checked = true))
     //   .catch((err) => console.err);
+    await page.waitFor(500);
     await page
       .$$eval('.selectable', (listEle) => {
-        listEle[listEle.length - 1].click();
+        listEle[listEle.length - 2].click();
       })
       .catch((err) => console.err);
-    await page.waitFor(500);
-    await page.select('select[name="QTimeHr"]', '10');
+    await page
+      .select('select[name="QTimeHr"]', '10')
+      .catch((err) => console.error(err));
     // searching btn
     await page.waitFor(500);
     await page.$eval('input[name=btnSubmit]', (button) => button.click());
-    //
-    await page.waitFor(1000);
+    await page.waitFor(500);
+    let time;
     await page
       .$$eval('.buttonEnable', (listEle) => {
         //loop through list ele and find substrings of times
         //saturday 10-11 bergen preferred
         for (let i = 0; i < listEle.length; i++) {
-          if (
-            listEle[i].attributes.onclick.nodeValue.includes('10:') ||
-            listEle[i].attributes.onclick.nodeValue.includes('9:') ||
-            listEle[i].attributes.onclick.nodeValue.includes('11:')
-          ) {
-            page.screenshot({ path: 'temp.png' });
-            keepGoing = false;
-            // listEle[i].click();
+          if (listEle[i].attributes.onclick.nodeValue.includes('2:')) {
+            listEle[i].click();
+            return 2;
           }
         }
+        for (let i = 0; i < listEle.length; i++) {
+          if (listEle[i].attributes.onclick.nodeValue.includes('3:')) {
+            listEle[i].click();
+            return 3;
+          }
+        }
+        for (let i = 0; i < listEle.length; i++) {
+          if (listEle[i].attributes.onclick.nodeValue.includes('4:')) {
+            listEle[i].click();
+            return 4;
+          }
+        }
+        return 0;
+      })
+      .then((data) => page.screenshot({ path: `${data}.png` }))
+      .then(() => {
+        keepGoing = false;
       })
       .catch((err) => console.err);
+
     //confirm btn
     // await page.$eval('input[name=btnSubmit]', (button) => button.click());
     // await browser.waitForTarget(() => false);
